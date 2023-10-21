88e1f12edeaec1b82176133a22d14249d8fafa77
akhilpandit09 <akhileshwarpandit0024@gmail.com>
2023-10-21 23:02:55 +0545

    Modified in GET.php file

--- Global Variables/superglobals/GET.php
+++ Global Variables/superglobals/GET.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>GET Global Variable</title>
 </head>
+
 <body>
     <a href="test_get.php?subject=PHP&web=W3schools.com">Test $GET</a>
 
     <?php
-echo "Study " . $_GET['subject'] . " at " . $_GET['web'];
-?>
+    echo "Study " . $_GET['subject'] . " at " . $_GET['web'];
+    ?>
 </body>
+
 </html>
